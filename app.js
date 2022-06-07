58d6876c0de6af1284fda5441932f3d8fbceed7c
cattybog <72874739+cattybog@users.noreply.github.com>
2022-06-06 22:50:08 -0500

    CORS Setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 const passport = require('passport');
+const cors = require("cors");
 
 var book = require('./routes/book');
 var app = express();
@@ -25,6 +26,23 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/books', express.static(path.join(__dirname, 'dist')));
 app.use('/book', book);
 app.use('/api/auth', auth);
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://kittenbook.software");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+  res.setHeader('Access-Control-Allow-Private-Network', "true");
+  next();
+});
+const corsOptions = {
+  origin: 'http://kittenbook.software',
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
